6c4efae4324c8af21f9a50e9a08e3de19e3e203c
Erick Gonçalves <erickbarros06@hotmail.com>
2023-02-27 23:29:44 +0000

    added comments

--- src/prompt.c
+++ src/prompt.c
@@ -6,12 +6,14 @@
 /*   By: egoncalv <egoncalv@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 21:42:33 by egoncalv          #+#    #+#             */
-/*   Updated: 2023/02/27 23:22:16 by egoncalv         ###   ########.fr       */
+/*   Updated: 2023/02/27 23:29:22 by egoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+// This function gives the prompt to the user, saves
+// the input to the history and returns it.
 char	*give_prompt()
 {
 	char	*input_line;
