4a27ccdf999fb68ac9f656572b202abcb435abc5
Erick Gonçalves <erickbarros06@hotmail.com>
2023-03-06 19:15:16 +0000

    added argc, argv, env

--- src/minishell.c
+++ src/minishell.c
@@ -6,17 +6,21 @@
 /*   By: egoncalv <egoncalv@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/25 21:00:34 by egoncalv          #+#    #+#             */
-/*   Updated: 2023/02/27 23:22:33 by egoncalv         ###   ########.fr       */
+/*   Updated: 2023/03/06 19:14:42 by egoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-int	main(void)
+int	main(int argc, char **argv, char **env)
 {
 	t_data	data;
 
-	while (1)
+	(void)argc; // Just so it can compile with flags, we can remove these lines after using the variables
+	(void)argv;
+	(void)env;
+
+	while (1) // While no exit() or ctrl+c / ctrl+d
 		data.cur_line = give_prompt();
-	return (1);
+	return (0);
 }
