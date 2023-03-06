ea811e083b985db1d9c09e1afcbe3258fc271aa7
Erick Gonçalves <erickbarros06@hotmail.com>
2023-03-06 19:16:30 +0000

    fix

--- src/prompt/prompt.c
+++ src/prompt/prompt.c
@@ -6,11 +6,11 @@
 /*   By: egoncalv <egoncalv@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 21:42:33 by egoncalv          #+#    #+#             */
-/*   Updated: 2023/02/27 23:29:22 by egoncalv         ###   ########.fr       */
+/*   Updated: 2023/03/06 19:16:21 by egoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/minishell.h"
+#include "../../include/minishell.h"
 
 // This function gives the prompt to the user, saves
 // the input to the history and returns it.
